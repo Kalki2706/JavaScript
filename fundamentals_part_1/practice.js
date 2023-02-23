4d3b6b01544de70df3c32ac64ef1bef664955bec
Kalki <kalkithewarrior2706@proton.me>
2023-02-23 19:34:15 +0530

    practice variable naming conventions

---
+++ fundamentals_part_1/practice.js
@@ -0,0 +1,11 @@
+let firstName = "Kalki";
+console.log(firstName);
+
+let dateOfBirth = 27 + "th " + "June";
+console.log(dateOfBirth);
+
+let _myjob = "Programmer";
+console.log(_myjob);
+
+let $mySkills = "HTML, CSS & JS";
+console.log($mySkills);
--- fundamentals_part_1/script.js
+++ fundamentals_part_1/script.js
@@ -21,9 +21,30 @@ console.log(firstName);
 // You can also write like first_name_person - it's used in ruby so Recommendation is use JS Standard
 // $ and _ are allowed in JS
 
+let jonas_maltida = "JM";
+let $function = 27;
+
+let person = "Jonas";
+let PI = 3.1415; //You can use PI in uppercase 'cause it's convention in JS.
+
 // You can't write like this:
 // 3years = 3 year
 // jonas&Matilda = "JM" //you can't use 'and' sign
 // let function = 27;
 // let new = 27; //new and fuction are reserved keywords
 // let name = "Jonas" //it's also a keyword even if it did'nt give error just don't use it us firstName instead.
+// let Person = "Jonas"; //don't use capital letter
+
+// DISCRIPTIVE VARIABLE NAMES
+
+// Write like this:-
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+// Not like this:-
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
