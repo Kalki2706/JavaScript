520b54150d9979dabfee83fec749f8d1b791aab4
Kalki <kalkithewarrior2706@proton.me>
2023-02-24 15:26:07 +0530

    types of keywords
    which you should use more and what you should avoid

--- fundamentals_part_1/script.js
+++ fundamentals_part_1/script.js
@@ -96,30 +96,75 @@ comment*/
 // shortcut for comments in VS code: "ctrl" + "/"
 
 // Boolean value
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
 // typeof operator
 // console.log(typeof true);
-console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 // console.log(typeof 23);
 // console.log(typeof "Jonas");
 
-javascriptIsFun = "YES!";
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
 // console.log(javascriptIsFun);
 
 // Undefined value
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-// Null value
+// // Null value
 
-console.log(null);
-console.log(typeof null);
+// console.log(null);
+// console.log(typeof null);
 // The null type will show as a object but it's just a bug and it's kept by JS for some legacy reasons
+
+// 3 different ways of declaring variables
+
+// let keyword
+let myJob = "Programmer";
+console.log(myJob);
+
+let mySkills = "Programmer";
+console.log(mySkills);
+
+// strict equality operator to show that different variables reference the same value.
+console.log(myJob === mySkills);
+
+// a mutation is the act of changing the properties of a value.
+myJob = "Web Developer";
+console.log(myJob);
+
+// During mutation use let keyword
+let age = 30;
+age = 31;
+
+console.log(age);
+
+// Const Keyword
+const birthYear = 1990;
+console.log(birthYear);
+
+// You can't change value of variable which used const keyword, it'll throw an error if you do, so.
+// birthYear = 1991;
+
+// You can't declare undefined variable to const.
+// const job;
+
+// *NOTE: You should use CONST keyword as much as you can
+
+// Var Keyword
+// You should avoid using this keyword
+
+var job = "programmer";
+job = "teacher";
+
+// Not using keyword is terrible idea even JS will not give error but the variable will save as globalobject
+lastName = "Schmedtmann";
+console.log(lastName);
+console.log(typeof lastName);
