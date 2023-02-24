1d1ab63b47c4c68ab6724aa15a66f09b51b06829
Kalki <kalkithewarrior2706@proton.me>
2023-02-24 13:12:41 +0530

    code commented

--- fundamentals_part_1/script.js
+++ fundamentals_part_1/script.js
@@ -1,31 +1,31 @@
-let js = "amazing";
+// let js = "amazing";
 // if (js == "amazing") alert("JS is Fun!");
 
 // 40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
 // Values & Variables
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Bob";
-console.log(firstName);
+// let firstName = "Bob";
+// console.log(firstName);
 
-firstName = "Jonas";
-console.log(firstName);
-console.log(firstName);
+// firstName = "Jonas";
+// console.log(firstName);
+// console.log(firstName);
 
 // JS standard to write like this
 // firstNamePerson - camelCase
 // You can also write like first_name_person - it's used in ruby so Recommendation is use JS Standard
 // $ and _ are allowed in JS
 
-let jonas_maltida = "JM";
-let $function = 27;
+// let jonas_maltida = "JM";
+// let $function = 27;
 
-let person = "Jonas";
-let PI = 3.1415; //You can use PI in uppercase 'cause it's convention in JS.
+// let person = "Jonas";
+// let PI = 3.1415; //You can use PI in uppercase 'cause it's convention in JS.
 
 // You can't write like this:
 // 3years = 3 year
@@ -39,15 +39,15 @@ let PI = 3.1415; //You can use PI in uppercase 'cause it's convention in JS.
 
 // Write like this:-
 
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
 
 // Not like this:-
 
-let job1 = "programmer";
-let job2 = "teacher";
+// let job1 = "programmer";
+// let job2 = "teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
 
 // DATA TYPES IN JS
 
