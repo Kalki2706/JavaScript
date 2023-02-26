c469d13660826dc048f775112525a9bbae151ec8
Kalki <kalkithewarrior2706@proton.me>
2023-02-26 19:09:57 +0530

    challenge one question

--- fundamentals_part_1/coding_challenge.js
+++ fundamentals_part_1/coding_challenge.js
@@ -1,2 +1,12 @@
 // BMI = mass / height ** 2 = mass / (height * height)
 // (mass in kg & height in meter)
+
+// 1. store Mark's and John's mass and height in variables
+// 2. Calculate both their BMI's using the formula you can even implement both varsions)
+// 3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+// TEST DATA 1 : Marks weights 78 kg and is 1.69 m tall.
+// John weight 92 kg and is 1.95 m tall.
+
+// TEST DATA 2 : Marks weights 95 kg and is 1.88 m tall.
+// John weights 85 kg & is 1.76 m tall.
