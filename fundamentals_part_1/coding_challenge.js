83646744b05fec751c7afc8a2248dcdd633a5b4c
Kalki <kalkithewarrior2706@proton.me>
2023-02-26 23:15:03 +0530

    test data 2nd solved by tutor

--- fundamentals_part_1/coding_challenge.js
+++ fundamentals_part_1/coding_challenge.js
@@ -69,10 +69,15 @@ console.log(markHigherBMI);
 
 // SOLVED BY TUTOR
 
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
 
 const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
